eaf63c2e13ffde2f3cd248c5399fdd2ce1b8a2dc
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-09-22 13:18:16 +0200

    fin job05 et job06

---
+++ jour01/job05/script.js
@@ -0,0 +1,13 @@
+
+function afficherjourssemaine(){
+    let jourssemaine = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+    
+    for (let i = 0; i < jourssemaine.length; i++) {
+
+        console.log (jourssemaine[i]);
+    }
+}
+
+afficherjourssemaine();
+
+
---
+++ jour01/job06/script.js
@@ -0,0 +1,22 @@
+
+function fizzbuzz(){
+    
+    for (let i = 0; i <= 151; i++) {
+         if (i % 3 == 0 && i % 5 == 0)
+            console.log (i + ' ' + 'FizzBuzz')
+        else if (i % 3 == 0 && 5 != 0)
+            console.log (i + ' ' + 'Fizz')
+        
+        else if (i % 5 == 0 && 3 != 0)
+            console.log (i + ' ' + 'Buzz')
+        
+        
+        else
+            console.log (i);
+
+    }
+}
+
+fizzbuzz();
+
+
