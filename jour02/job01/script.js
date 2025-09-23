13f3e7664dd5ae1aa1b543b6628a92a1eb2e3ff2
Nacer-atoui <nacer.atoui@laplateforme.io>
2025-09-23 16:31:32 +0200

    fin jour 02 job01

---
+++ jour02/job01/script.js
@@ -0,0 +1,9 @@
+function citation() {
+
+    let c = document.getElementById("citation").textContent;
+
+    console.log(c);
+    
+}
+
+document.getElementById("button").addEventListener("click", citation);
